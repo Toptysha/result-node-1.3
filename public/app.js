ee6a96d6317d7c916b9c3a53778e47f51001cb73
Vladislav Baranov <toptun113@gmail.com>
2024-04-14 12:55:11 +0300

    commit for task 1.4

--- public/app.js
+++ public/app.js
@@ -1,14 +1,45 @@
 document.addEventListener("click", (e) => {
   if (e.target.dataset.type === "edit") {
     const id = e.target.dataset.id;
-    const titleFromPrompt = prompt("New title?");
+    let liParent = e.target.closest("li");
+    let titleFromHTML = e.target.closest("li").querySelector("p").textContent;
 
-    if (titleFromPrompt !== null) {
-      edit(id, titleFromPrompt).then(() => {
-        let titleFromHTML = e.target.closest("li").querySelector("p");
-        titleFromHTML.textContent = titleFromPrompt;
+    let copyLiParent = liParent.cloneNode(true);
+
+    liParent.innerHTML = ``;
+
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("value", titleFromHTML);
+    liParent.appendChild(input);
+
+    let divButtons = document.createElement("div");
+    liParent.appendChild(divButtons);
+
+    let acceptButton = document.createElement("button");
+    acceptButton.setAttribute("class", "btn btn-success m-1");
+    acceptButton.textContent = "Accept";
+    divButtons.appendChild(acceptButton);
+    acceptButton.addEventListener("click", (event) => {
+      let titleFromInput = input.value;
+
+      let newLiParent = event.target.closest("li");
+
+      edit(id, titleFromInput).then(() => {
+        newLiParent.innerHTML = copyLiParent.innerHTML;
+        let titleForHTML = newLiParent.querySelector("p");
+        titleForHTML.textContent = titleFromInput;
       });
-    }
+    });
+
+    let cancelButton = document.createElement("button");
+    cancelButton.setAttribute("class", "btn btn-danger m-1");
+    cancelButton.textContent = "Cancel";
+    divButtons.appendChild(cancelButton);
+    cancelButton.addEventListener("click", (event) => {
+      let newLiParent = event.target.closest("li");
+      newLiParent.innerHTML = copyLiParent.innerHTML;
+    });
   }
 
   if (e.target.dataset.type === "remove") {
